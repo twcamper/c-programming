52053a5b1ac78f94445d11c9ad5642c35d398d54
tim <twcamper@thoughtworks.com>
2013-02-01 16:27:38 -0600

    2.8.6 - bug somewhere: the 2.8.5 and 2.8.6 algorithms produce different results

--- 02/polynomial.c
+++ 02/polynomial.c
@@ -3,7 +3,7 @@
 int main(void)
 {
   int x, tmp;
-
+  int horners_result;
   int cf1, cf2, cf3, cf4, cf5, cf6; /* coefficients */
 
   printf("3x^5 + 2x^4 - 5x^3 - x^2 + x7 - 6\n");
@@ -21,6 +21,26 @@ int main(void)
   cf6 = 6;
 
   printf("result: %d\n",  cf1 + cf2 - cf3 - cf4 + cf5 - cf6);
+  printf("Horner's rule: ((((3x + 2)x - 5)x - 1)x + 7)x - 6\n");
+
+  /*Not sure where the mistake is, but the 2 algorithms (above, and the Horner's rule version below) produce different results*/
+
+  /*Both versions below give the same result:*/
+  /*
+  horners_result = (
+      ( (
+        ( (
+          ( (
+            ( (3 * x) + 2) * x
+           ) - 5) * x
+         ) - 1) * x
+       ) + 7) * x
+      ) - 6;
+      */
+
+  horners_result = (((((3 * x) + 2) * x - 5) * x - 1) * x + 7) * x - 6;
+
+  printf("result: %d\n", horners_result);
 
   return 0;
 }
