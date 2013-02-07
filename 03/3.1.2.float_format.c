a30b03eb3dd497976526dbc618b84d8fbe29f773
Tim Camper <twcamper@thoughtworks.com>
2013-02-07 14:19:08 -0600

    ex 3.1.2 added

--- 03/3.1.2.float_format.c
+++ 03/3.1.2.float_format.c
@@ -2,11 +2,18 @@
 
 int main(void)
 {
-
+  printf("Exercise 3.1.1\n");
   printf("%6d, %4d\n", 86, 1040);
   printf("%12.5e\n", 30.253);
   printf("%.4f\n", 83.162);
   printf("%-6.2g\n", .0000009979);
+
+
+  printf("\nExercise 3.1.2\n");
+
+  float x = 12.9f;
+  printf("|%-7.1e|%4.6e|%-5.3f|%6.0f|\n", x, x, x, x);
+
 
   return 0;
 }
