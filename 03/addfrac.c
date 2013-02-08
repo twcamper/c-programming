c56e4e7d7d6ea4d9aa6d3ff30d1cbe39679a3016
tim <twcamper@thoughtworks.com>
2013-02-07 18:42:01 -0600

    pgm 6 - refine fraction adder

--- 03/addfrac.c
+++ 03/addfrac.c
@@ -4,11 +4,8 @@ int main(void)
 {
   int num1, num2, denom1, denom2, result_num, result_denom;
 
-  printf("Enter first fraction: ");
-  scanf("%d / %d", &num1, &denom1);
-
-  printf("Enter second fraction: ");
-  scanf("%d / %d", &num2, &denom2);
+  printf("Enter two fractions separated by a plus sign: ");
+  scanf("%d / %d + %d / %d", &num1, &denom1, &num2, &denom2);
 
   result_num = num1 * denom2 + num2 * denom1;
   result_denom = denom1 * denom2;
