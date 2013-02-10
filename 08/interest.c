3f9811f40e82f590fa1f43ce7eb1204ba6ed8e7d
tim <twcamper@thoughtworks.com>
2013-02-10 17:09:59 -0600

    pgm 8.5 - compound the interest monthly, yet display the annual balance

--- 08/interest.c
+++ 08/interest.c
@@ -5,7 +5,8 @@
 
 int main(void)
 {
-  int i, low_rate, num_years, year;
+  int i, m, low_rate, num_years, year;
+  /*hold 5 balances: a working value for each of 5 rates we'll be compounding*/
   double value[5];
 
   printf("Enter interest rate: ");
@@ -15,7 +16,10 @@ int main(void)
 
   printf("\nYears");
   for (i = 0; i < NUM_RATES; i++) {
+    /* calculate the 4 remaining rates*/
+    /* and display the 5 rates in a header row*/
     printf("%6d%%", low_rate + i);
+    /*for each rate, initialize its working array element to $100.00*/
     value[i] = INITIAL_BALANCE;
   }
   printf("\n");
@@ -23,7 +27,12 @@ int main(void)
   for (year = 1; year <= num_years; year++)  {
     printf("%3d    ", year);
     for (i = 0; i < NUM_RATES; i++ ) {
-      value[i] += ( low_rate + i) / 100.00 * value[i];
+      /*to begin the series of monthly balance calcuations, save off the previous year's balance*/
+      /*for the rate we're currently on*/
+      for (m = 0; m < 12; m++) {
+        value[i] += ((( low_rate + i) / 100.00) / 12.0) * value[i];
+      }
+      /*accumulated balance for a year of 12 monthtly calcuations: save for display and to seed next year's calc for this rate*/
       printf("%7.2f", value[i]);
     }
   printf("\n");
@@ -31,19 +40,3 @@ int main(void)
 
   return 0;
 }
-
-/*
-declare working integer variables
-declare array to hold 5 balances: a working value for each of 5 rates we'll be compounding anually
-ask user for a rate which will be used as the first as our series of 5 which increase incrementally by 1%
-ask user for a number of years over to which to compound the interest for each of the 5 rates
-calculate the 4 remaining rates
-display the 5 rates in a header row
-for each rate, initialize its working array element to $100.00
-for each year
-    display the year in the header col
-        for each rate
-            calculate and display the new annual balance based on the balance stored in the rate's array element, and replace
-            the old array element value with the new one for use in the next year's iteration for that same rate
-
-*/
