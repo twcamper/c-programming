87c14fb0c7e40b68c42da7d2687ded3832bdde42
tim <twcamper@thoughtworks.com>
2013-02-10 11:24:38 -0600

    pgm 8.3 - loop input to digit counter

--- 08/repdigit.c
+++ 08/repdigit.c
@@ -1,31 +1,36 @@
 #include <stdio.h>
 #define DIGIT_FORMAT "%3d"
+#define NUM_DIGITS 10
 int main(void)
 {
-  int digit_counts[10] = {0};
+  int digit_counts[NUM_DIGITS];
   int digit;
   long n;
 
-  printf("Enter a number: ");
-  scanf("%ld", &n);
+  printf("Enter a number (0 or negative to terminate: ");
+  while (scanf("%ld", &n), (n > 0)) {
+    for (digit = 0; digit < NUM_DIGITS; digit++) {
+      digit_counts[digit] = 0;
+    }
 
-  while (n > 0)  {
-    digit = n % 10;
-    digit_counts[digit]++;
-    n /= 10;
-  }
+    while (n > 0)  {
+      digit = n % 10;
+      digit_counts[digit]++;
+      n /= 10;
+    }
 
-  printf("Digit:\t\t");
-  for (digit = 0; digit < 10; digit++) {
-    printf(DIGIT_FORMAT, digit);
-  }
-  puts("");
+    printf("Digit:\t\t");
+    for (digit = 0; digit < NUM_DIGITS; digit++) {
+      printf(DIGIT_FORMAT, digit);
+    }
+    puts("");
 
-  printf("Occurrences:\t");
-  for (digit = 0; digit < 10; digit++) {
-    printf(DIGIT_FORMAT, digit_counts[digit]);
+    printf("Occurrences:\t");
+    for (digit = 0; digit < NUM_DIGITS; digit++) {
+      printf(DIGIT_FORMAT, digit_counts[digit]);
+    }
+    puts("");
   }
-  puts("");
 
   return 0;
 }
