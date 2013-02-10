d97b6f5a88f987c95e042b7612b4f8814cc22d4e
tim <twcamper@thoughtworks.com>
2013-02-10 15:48:18 -0600

    pgm 8.5 - psudeo out the original to help me understand the algorithm

--- 08/interest.c
+++ 08/interest.c
@@ -31,3 +31,19 @@ int main(void)
 
   return 0;
 }
+
+/*
+declare working integer variables
+declare array to hold 5 balances: a working value for each of 5 rates we'll be compounding anually
+ask user for a rate which will be used as the first as our series of 5 which increase incrementally by 1%
+ask user for a number of years over to which to compound the interest for each of the 5 rates
+calculate the 4 remaining rates
+display the 5 rates in a header row
+for each rate, initialize its working array element to $100.00
+for each year
+    display the year in the header col
+        for each rate
+            calculate and display the new annual balance based on the balance stored in the rate's array element, and replace
+            the old array element value with the new one for use in the next year's iteration for that same rate
+
+*/
