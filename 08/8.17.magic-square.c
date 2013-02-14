38ea28c8c6bf930a08e79966596fdf01307e4878
tim <twcamper@thoughtworks.com>
2013-02-14 17:40:51 -0600

    pgm 8.17 - magic square problem completed

--- 08/8.17.magic-square.c
+++ 08/8.17.magic-square.c
@@ -2,7 +2,8 @@
 
 int main(void)
 {
-  int n;
+  int n, size, i, new_row, new_col;
+  int row, col;
 
   printf("This program creates a magic square of a specified size.\nThe size must be an odd number between 1 and 99\n");
   printf("Enter the size of the magic square: ");
@@ -14,7 +15,60 @@ int main(void)
     return 1;
   }
 
-  char square[n][n];
+  int square[n][n];
+
+  /*initialize to all zeroes*/
+  for (row = 0; row < n; row++) {
+    for (col = 0; col < n; col++)
+      square[row][col] = 0;
+  }
+
+  /*Start by placing the number 1 in the middle of row 0*/
+  row = 0;
+  col = n / 2;
+
+  size = n * n;
+  for (i = 1; i <= size; i++) {
+    square[row][col] = i;
+    /*
+     * Find the new element
+     */
+    new_row = row;
+    new_col = col;
+
+    /*wrap around by resetting the row if necessary*/
+    if (new_row == 0)
+      new_row = n;
+    /*move up by 1*/
+    new_row--;
+
+    /*wrap around by resetting the column if necessary*/
+    if (new_col ==  n - 1)
+      new_col = -1;
+    /*move to the right by 1*/
+    new_col++;
+
+    /* is the new element available? */
+    if (square[new_row][new_col] == 0) {
+      row = new_row;
+      col = new_col;
+    } else {
+      /*
+       * if the element is already occupied, put the number directly below
+       * the previously stored number
+       */
+      if (row == n - 1)
+        row = - 1;
+      /*move down by 1*/
+      row++;
+    }
+  }
+
+  for (row = 0; row < n; row++) {
+    for (col = 0; col < n; col++)
+      printf("%d\t", square[row][col]);
+    printf("\n");
+  }
 
   return 0;
 }
