074511c7c5ac87b6d9f0addbf981f5f34a87bdf8
tim <twcamper@thoughtworks.com>
2013-02-16 18:36:32 -0600

    documenting anomaly: gcc-4.7.2 bug??

---
+++ weird/scanf-clobbers-adjacent-var.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int play_game(void);
+
+int main(void)
+{
+  char response;
+  int wins, losses;
+  wins = losses = 0;
+
+  do {
+    if (play_game()) {
+      printf("Win!\n");
+      wins++;
+    } else {
+      printf("Lose.\n");
+      losses++;
+    }
+
+    printf("Play again? ");
+    /* 
+     * this format string (" %c") works, and causes no problems
+     */ 
+    /*scanf(" %c", &response);*/
+
+    /*
+     * with this format string, (" %s") 
+     * the value of 'losses' gets reset to 0
+     */ 
+    scanf(" %s", &response);
+  } while ( response == 'y' || response == 'Y');
+
+  printf("Wins: %d\tLosses: %d\n", wins, losses);
+
+  return 0;
+}
+
+int play_game(void)
+{
+  return rand() % 2;
+}
