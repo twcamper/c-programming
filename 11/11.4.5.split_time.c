f3541902421a9324cf08e622a5edc0e96defc574
tim <twcamper@thoughtworks.com>
2013-02-25 09:57:14 -0600

    algorithmic "doh!"

--- 11/11.4.5.split_time.c
+++ 11/11.4.5.split_time.c
@@ -1,19 +1,26 @@
 #include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 void split_time(long total_sec, int *hr, int *min, int *sec)
 {
   *hr  = total_sec / ( 60 * 60 );
-  *min = *hr % 60;
+  *min = total_sec % ( 60 * 60 ) / 60;
   *sec = total_sec % 60;
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
   int hr, min, sec;
 
-  /* 1 hour, 1 min, 1 sec */
-  split_time(3661, &hr, &min, &sec);
+  if (argc > 1) {
+    /* 1 hour, 1 min, 1 sec */
+    split_time(atoi(argv[1]), &hr, &min, &sec);
+    printf("%d:%d:%d\n", hr, min, sec);
+  }
+
   /* now */
   split_time(time(NULL), &hr, &min, &sec);
+
   return 0;
 }
