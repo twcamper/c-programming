e1d52c3f024e962c9d8919c033cc28a1094fc3b9
tim <twcamper@thoughtworks.com>
2013-02-27 23:35:08 -0600

    ex 12.4.15 -- access elements of a row via pointer  (print temps table)

--- 12/12.3.7.search.c
+++ 12/12.3.7.search.c
@@ -1,6 +1,37 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+#define DAYS 7
+#define HOURS 24
+
+int temperatures[DAYS][HOURS];
+
+bool search(const int *a, int n, int key);
+bool search_temps(int key);
+void generate_temperatures(void);
+void print_temperatures(int day);
+
+int main(void)
+{
+  int a[] = {123,456, 99, 0, -12, 77};
+
+  printf("found 99?: %d\n", search(a, 6, 99));
+  printf("found 199?: %d\n", search(a, 6, 199));
+
+  generate_temperatures();
+  *(temperatures[0] + (DAYS * HOURS / 2)) = 32;
+  /* temperatures[DAYS/2][HOURS / 2] = 32; */
+
+  printf("found 32?: %d\n", search_temps(32));
+  printf("found 30?: %d\n", search_temps(30));
+  
+  for (int d = 0; d < DAYS; d++)
+    print_temperatures(d);
+
+  return 0;
+}
 bool search(const int a[], int n, int key)
 {
   const int *pa;
@@ -13,30 +44,25 @@ bool search(const int a[], int n, int key)
 
 bool search_temps(int t)
 {
-  int temperatures[7][24] = {
-    {23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23},
-    {23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23},
-    {23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23},
-    {23,23,23,23,23,23,23,23,23,23,32,23,23,23,23,23,23,23,23,23,23,23,23,23},
-    {23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23},
-    {23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23},
-    {23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23,23}
-  };
 
   int row;
-  for (row = 0; row < 7; row++)
-    if (search(temperatures[row], 24, t))
+  for (row = 0; row < DAYS; row++)
+    if (search(temperatures[row], HOURS, t))
       return true;
   return false;
 }
-int main(void)
+void generate_temperatures(void)
 {
-  int a[] = {123,456, 99, 0, -12, 77};
+  srand((unsigned) time(NULL));
 
-  printf("found 99?: %d\n", search(a, 6, 99));
-  printf("found 199?: %d\n", search(a, 6, 199));
-
-  printf("found 32?: %d\n", search_temps(32));
-  printf("found 30?: %d\n", search_temps(30));
-  return 0;
+ for (int row = 0; row < DAYS; row++)
+   for (int col = 0; col < HOURS; col++)
+     temperatures[row][col] = rand() % 88;
+}
+void print_temperatures(int day)
+{
+  int *h_ptr;
+  for (h_ptr = temperatures[day]; h_ptr < temperatures[day] + HOURS; h_ptr++)
+    printf("%2d ", *h_ptr);
+  printf("\n");
 }
