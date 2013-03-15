b51fb542dd91c8245b2377319f5d08689ea9be48
Tim Camper <twcamper@thoughtworks.com>
2013-03-15 13:24:15 -0500

    ex 16.3.10 - rectangle functions done TDD style!

--- include/rectangle.h
+++ include/rectangle.h
@@ -7,7 +7,9 @@ typedef struct {int x, y;} point;
 typedef struct {point upper_left, lower_right; } rectangle;
 
 int area(rectangle r);
-int center(rectangle r);
+point center(rectangle r);
 rectangle move(rectangle r, int x, int y);
 bool is_inside(rectangle r, point p);
+int height(rectangle r);
+int width(rectangle r);
 #endif
--- lib/rectangle.c
+++ lib/rectangle.c
@@ -2,17 +2,37 @@
 
 int area(rectangle r)
 {
-  return 0;
+  return height(r) * width(r);
 }
-int center(rectangle r)
+point center(rectangle r)
 {
-  return 0;
+  return (point) {width(r) / 2, height(r) / 2};
 }
 rectangle move(rectangle r, int x, int y)
 {
-  return (rectangle) {(point) {0,0}, (point) {0,0}};
+  r.upper_left.x += x;
+  r.lower_right.x += x;
+  r.upper_left.y += y;
+  r.lower_right.y += y;
+
+  return r;
 }
 bool is_inside(rectangle r, point p)
 {
+  if (p.x >= r.upper_left.x &&
+      p.x <= r.lower_right.x &&
+      p.y >= r.upper_left.y &&
+      p.y <= r.lower_right.y) {
+    return true;
+  }
+
   return false;
+}
+int height(rectangle r)
+{
+  return r.lower_right.y - r.upper_left.y;
+}
+int width(rectangle r)
+{
+  return r.lower_right.x - r.upper_left.x;
 }
