7cdbe9f21bb49c3e8eda279fd27e7e6b12817749
Tim Camper <twcamper@thoughtworks.com>
2013-03-21 14:27:49 -0500

    ch 17 list exercises - test of create list helper func() passes

---
+++ 17/17.5-list-exercises.c
@@ -0,0 +1,62 @@
+#include "test_runner.h"
+#include <stdlib.h>
+
+typedef struct node {
+  int value;
+  struct node *next;
+} node;
+
+node *create(void)
+{
+  node *n;
+  if ((n = malloc(sizeof(node))) == NULL) {
+    fprintf(stderr, "malloc failed: %s:%d\n", __FILE__, __LINE__);
+    exit(EXIT_FAILURE);
+  }
+  return n;
+}
+node *add(node *list, int value)
+{
+  node *new = create();
+  new->value = value;
+  new->next = list;
+  return new;
+}
+node *create_list(int a[], int n)
+{
+  node *first = create();
+  first->value = a[0];
+  for (int i = 1; i < n; i++) {
+    first = add(first, a[i]);
+  }
+  return first;
+}
+void free_list(node *list)
+{
+
+}
+int free_list_test(void)
+{
+
+  return 0;
+}
+int create_list_test(void)
+{
+  node *list = create_list((int[]){11, 22, 33, 44, 55, 66, 77, 88, 99, 101}, 10);
+  _assert(list->next != NULL);
+  _assert(list->value == 101);
+  _assert(list->next->value == 99);
+  return 0;
+}
+int delete_from_list_test(void)
+{
+
+  return 0;
+}
+
+int all_tests(void)
+{
+
+  _run(create_list_test);
+  return 0;
+}
