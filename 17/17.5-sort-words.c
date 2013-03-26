34901e223fd1af3f36c0ab19de5e5b57dcf5c885
Tim Camper <twcamper@thoughtworks.com>
2013-03-26 18:49:28 -0500

    pgm 17.6 - qsort version of previous

--- 17/17.5-sort-words.c
+++ 17/17.5-sort-words.c
@@ -2,15 +2,22 @@
 #include <stdlib.h>
 #include "readline.h"
 #include "error.h"
+#include <assert.h>
 
 #define MAX_WORDS 1024
 #define WORD_LEN  28
+int compare_words(const void *a, const void *b)
+{
+  char **string_a = (char **)a;
+  char **string_b = (char **)b;
 
+  return strcmp(*string_a, *string_b);
+}
 int main(void)
 {
   char input[WORD_LEN + 1];
   char *words[MAX_WORDS];
-  int i, word_count = 0;
+  int word_count = 0;
 
   for(;;) {
     if (word_count == MAX_WORDS) {
@@ -23,25 +30,18 @@ int main(void)
     if (input[0] == '0')
       break;
 
-    /* find alphabetic position to insert word into */
-    for (i = 0; i < word_count; i++)
-      if (strcmp(input, words[i]) < 0)
-        break;
-
-    /* shift everybody else down to open up a hole at i*/
-    for (int j = word_count; j > i; j--)
-      words[j] = words[j - 1];
-
     /* on my 64 bit machine, malloc will probably allocate nearly an MB anyway! */
-    words[i] = malloc(strlen(input) + 1);
-    if (!words[i])
+    words[word_count] = malloc(strlen(input) + 1);
+    if (!words[word_count])
       memory_error(__FILE__, __LINE__, __func__);
 
-    strcpy(words[i], input);
+    strcpy(words[word_count], input);
     word_count++;
   }
 
-  for (i = 0; i < word_count; i++) {
+  qsort(words, word_count, sizeof(char *), compare_words);
+
+  for (int i = 0; i < word_count; i++) {
     printf("%s\n", words[i]);
     free(words[i]);
   }
