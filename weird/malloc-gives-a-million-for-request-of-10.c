15e23ab86503d075d6efcded2bae77d96ab88fe0
Tim Camper <twcamper@thoughtworks.com>
2013-03-26 09:33:52 -0500

    documenting overallocation by 5 orders of magnitude

---
+++ weird/malloc-gives-a-million-for-request-of-10.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int test_1(void)
+{
+  char *s;
+  int i;
+
+  assert(sizeof(char) == 1);
+
+  s = malloc(10);
+  assert(s != NULL);
+
+  s[10] = 'A';
+  assert(s[10] == 'A');
+
+  /* Threshhold on my machine */
+  i = 1048447;  /* works */
+  i = 1048448;  /* segfualts */
+
+  i = 1000000;
+  s[i] = 'A';
+  assert(s[i] == 'A');
+
+  free(s);
+
+  return 0;
+}
+
+int test_2(void)
+{
+  char *s;
+  int i = 0;
+
+  s = malloc(10);
+  assert(s != NULL);
+
+  for (;i < 100000; i++)
+    strcat(s, "0123456789");
+
+  puts(s);
+
+  free(s);
+
+  return 0;
+}
+
+int main(void)
+{
+  assert(test_1() == 0);
+  assert(test_2() == 0);
+
+  return 0;
+}
