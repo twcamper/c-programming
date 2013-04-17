07cd77cccde20d10f3252361e7d0979bc5418b64
tim <twcamper@thoughtworks.com>
2013-04-16 20:06:53 -0500

    ex 22.5.14 - my fgets works, passes tests, though it's uglier than King's

--- 22/22.5-line-io.c
+++ 22/22.5-line-io.c
@@ -3,6 +3,23 @@
 #include <assert.h>
 
 
+char *fugats(char *s, int n, FILE *stream)
+{
+  int i = 0, ch = fgetc(stream);
+  if (ftell(stream) > 0 && (feof(stream) || ferror(stream)))
+    return NULL;
+  else
+    ungetc(ch, stream);
+
+  while (i < n - 1 && (ch = fgetc(stream)) != EOF) {
+    s[i++] = ch;
+    if (ch == '\n')
+      break;
+  }
+  s[i] = '\0';
+
+  return s;
+}
 int empty_test(char *(*line_reader)(char *, int, FILE *))
 {
   FILE *fp = fopen("data/empty.txt", "r");
@@ -10,6 +27,19 @@ int empty_test(char *(*line_reader)(char *, int, FILE *))
 
   assert(fp != NULL);
   assert(line_reader(line, 1, fp) == line);
+
+  fclose(fp);
+  return 0;
+}
+int no_newline_test(char *(*line_reader)(char *, int, FILE *))
+{
+  FILE *fp = fopen("data/no-newline.txt", "r");
+  char line[10];
+
+  assert(fp != NULL);
+  assert(line_reader(line, 10, fp) != NULL);
+  assert(strcmp(line, "no way") == 0);
+  assert(line_reader(line, 10, fp) == NULL);
 
   fclose(fp);
   return 0;
@@ -43,12 +73,30 @@ int truncate_before_newline_test(char *(*line_reader)(char *, int, FILE *))
   fclose(fp);
   return 0;
 }
+int full_file(void)
+{
+  FILE *fp = fopen(__FILE__, "r");
+  char line[255];
+
+  while (fugats(line, 255, fp)) {
+    printf("%s", line);
+  }
+  return 0;
+}
 int main(void)
 {
 
   empty_test(fgets);
+  no_newline_test(fgets);
   one_liner_test(fgets);
   truncate_before_newline_test(fgets);
+
+  empty_test(fugats);
+  no_newline_test(fugats);
+  one_liner_test(fugats);
+  truncate_before_newline_test(fugats);
+
+  assert(full_file() == 0);
 
   return 0;
 }
