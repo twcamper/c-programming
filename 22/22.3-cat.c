87386700db240be190c5d2517537caa117064618
tim <twcamper@thoughtworks.com>
2013-04-17 18:54:04 -0500

    pgm 22.3, cat: add option to number non-blank lines

--- 22/22.3-cat.c
+++ 22/22.3-cat.c
@@ -7,13 +7,14 @@
 
 typedef struct options
 {
-  bool number;
+  bool number_all;
+  bool number_non_blank;
 } Options;
 
 
 static void usage(char *pgm)
 {
-  fprintf(stderr, "Usage:\v$ %s [-n] [file ...]\n", pgm);
+  fprintf(stderr, "Usage:\v$ %s [-nb] [file ...]\n", pgm);
 }
 static void print_e(int e, char *program, char *file)
 {
@@ -26,19 +27,25 @@ static void print_n(int *lines)
 }
 static Options process_options(int *argc, char **argv[])
 {
-  Options options = {false};
+  Options options = {false, false};
   int ch;
 
-  while ((ch = getopt(*argc, *argv, "n")) != EOF) {
+  while ((ch = getopt(*argc, *argv, "nb")) != EOF) {
     switch(ch) {
       case 'n':
-        options.number = true;
+        options.number_all = true;
+        break;
+      case 'b':
+        options.number_non_blank = true;
         break;
       default:
         usage(*argv[0]);
         exit(EXIT_FAILURE);
     }
   }
+  if (options.number_non_blank)
+    options.number_all = false;
+
   *argc -= optind;
   *argv += optind;
 
@@ -47,7 +54,7 @@ static Options process_options(int *argc, char **argv[])
 int main(int argc, char *argv[])
 {
   FILE *fp;
-  int lines, ch;
+  int lines, ch, next_char;
   char *program = argv[0];
   Options o = process_options(&argc, &argv);
 
@@ -62,7 +69,7 @@ int main(int argc, char *argv[])
       continue;
     }
 
-    if (o.number)
+    if (o.number_all || o.number_non_blank)
       lines = 1;
 
     for(;;) {
@@ -72,16 +79,30 @@ int main(int argc, char *argv[])
         }
         break;
       }
-      if (o.number) {
+      if (o.number_all) {
         if (ftell(fp) == 1) {
           print_n(&lines);
         }
         if (ch == '\n') {
           putchar(ch);
-          if ((ch = fgetc(fp)) != EOF)
+          next_char = fgetc(fp);
+          ungetc(next_char, fp);
+          if (next_char != EOF)
             print_n(&lines);
-          ungetc(ch, fp);
           continue;
+        }
+      }
+      if (o.number_non_blank) {
+        next_char = fgetc(fp);
+        ungetc(next_char, fp);
+        if (ch == '\n') {  /* this could be the first the line also */
+          putchar(ch);
+          if (next_char != EOF && next_char != '\n')
+            print_n(&lines);
+          continue;
+        }
+        if (ftell(fp) == 1 && next_char != '\n') {  /* first line, unless it's blank */
+          print_n(&lines);
         }
       }
       putchar(ch);
