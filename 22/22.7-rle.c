64eee9b10fbc8b263472a1e260d79554defa1df6
tim <twcamper@thoughtworks.com>
2013-04-19 15:01:17 -0500

    pgm 22.7, run length encoder: block IO works, encoding yet to be written

--- 22/22.7-rle.c
+++ 22/22.7-rle.c
@@ -5,10 +5,35 @@
 /* Run Length Encoding */
 #define EXT ".rle"
 #define EXT_LEN 4
-
+#define BUFFER_SIZE BUFSIZ
 static void encode(FILE *in, FILE *out)
 {
+  /* fread proved unreliable with int elements */
+  char inbuffer[BUFFER_SIZE] = {0};
+  char outbuffer[BUFFER_SIZE] = {0};
+  size_t i;
+  size_t n_read = 0, n_written = 0;
 
+  for (;;) {
+    if ((n_read = fread(inbuffer, sizeof(inbuffer[0]), BUFFER_SIZE, in)) < BUFFER_SIZE) {
+      if (ferror(in)) {
+        print_error(errno, __FILE__, "fread(in)");
+        return;
+      }
+      if (feof(in) && n_read == 0)
+        break;
+    }
+
+    for (i = 0; i < n_read; i++)
+      outbuffer[i] = inbuffer[i];
+
+    if ((n_written = fwrite(outbuffer, sizeof(outbuffer[0]), n_read, out)) < n_read) {
+      if (ferror(out)) {
+        print_error(errno, __FILE__, "fread(out)");
+        return;
+      }
+    }
+  }
 }
 
 int main(int argc, char *argv[])
@@ -16,8 +41,8 @@ int main(int argc, char *argv[])
   if (argc != 2)
     invocation_error(argv[0], "[file]");
 
-  FILE *infile;
-  if ((infile = fopen(argv[1], "rb")) == NULL)
+  FILE *istream;
+  if ((istream = fopen(argv[1], "rb")) == NULL)
     exit_error(errno, argv[0], argv[1]);
 
   /*
@@ -28,22 +53,26 @@ int main(int argc, char *argv[])
   strcpy(outfile_name, argv[1]);
   strncat(outfile_name, EXT, EXT_LEN + 1);
 
-  FILE *outfile;
-  if ((outfile = fopen(outfile_name, "wb")) == NULL)
-    exit_error(errno, argv[0], outfile_name);
+  FILE *ostream;
+  if ((ostream = fopen(outfile_name, "wb")) == NULL) {
+    print_error(errno, argv[0], outfile_name);
+    if (fclose(istream) == EOF)
+      exit_error(errno, argv[0], argv[1]);
+    exit(EXIT_FAILURE);
+  }
 
   /*
    * Process
    */
-  encode(infile, outfile);
+  encode(istream, ostream);
 
   /*
    * Close Files
    */
-  if (fclose(infile) == EOF)
-    exit_error(errno, argv[0], argv[1]);
+  if (fclose(istream) == EOF)
+    print_error(errno, argv[0], argv[1]);
 
-  if (fclose(outfile) == EOF)
+  if (fclose(ostream) == EOF)
     exit_error(errno, argv[0], outfile_name);
 
   return 0;
