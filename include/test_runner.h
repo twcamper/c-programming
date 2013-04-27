0caa1dfbdf072b69993f2deb24b3d37ee5afcb85
Tim Camper <twcamper@thoughtworks.com>
2013-04-26 19:18:05 -0500

    more concise test output, thank you very much

--- include/test_runner.h
+++ include/test_runner.h
@@ -1,13 +1,16 @@
 /* Thanks to Eric Radman, bitbucket.org/eradman/tdd-in-c  */
 #include <stdio.h>
 
+#define RED       "\033[0;36m"
+#define GREEN     "\033[0;32m"
+#define END_COLOR "\033[0m"
 int tests_run = 0;
 int assertions = 0;
 
 #define _assert(e)            \
         do {                  \
           if (!(e)) {         \
-            printf("%s:%u (%s) failed assertion:\v(%s)\n", __FILE__, __LINE__, __func__, #e);  \
+            printf("%s%s:%u (%s) failed assertion:\v(%s)\n", RED, __FILE__, __LINE__, __func__, #e);  \
             return 1;         \
           } else {            \
             assertions++;     \
@@ -22,11 +25,16 @@ int assertions = 0;
 
 static int all_tests(void);
 
-int main(void) {
+int main(int argc, char *argv[]) {
+  if (argc < 0)
+    puts("Would the compiler please shut up about unused argc?");
+
 	int result = all_tests();
 	if (result == 0)
-		printf("PASSED\n");
-	printf("Tests: %d\nAssertions: %d\n\n", tests_run, assertions);
+		printf("%sPASS\t", GREEN);
+  else
+    printf("%sFAIL\t", RED);
+	printf("Tests: %d\tAssertions: %d\t%s\n%s", tests_run, assertions, argv[0], END_COLOR);
 
 	return result != 0;
 }
