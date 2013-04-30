6bcaa904a0709ff3eeeb5c80ebb52f599e0dd2dd
Tim Camper <twcamper@thoughtworks.com>
2013-04-30 17:57:19 -0500

    pgm 22.9, merge parts record files

--- 22/22.9-merge-parts.c
+++ 22/22.9-merge-parts.c
@@ -1,134 +1,159 @@
 #if defined(__linux__) || defined(__linux) || defined(__gnu_linux__)
-#include <sys/types.h>  /* for off_t */
+  #include <sys/types.h>  /* for off_t */
 #endif
 #include <stdio.h>
+#include <stdbool.h>
 #include <sys/stat.h>
 #include "error.h"
 #include "part-type.h"
 
+typedef struct file_info {
+  char *name;//name[FILENAME_MAX];
+  off_t size;
+  size_t count;
+} FileInfo;
+
+static void validate_input_file(char *f, struct stat *fs, size_t record_size)
+{
+  if (stat(f, fs) !=0)
+    exit_error(errno, __FILE__, f);
+
+  if (fs->st_size == 0 || fs->st_size % record_size) {
+    fprintf(stderr, "Empty or corrupt file '%s': size must be multiple of %ld\n",
+        f,
+        (size_t)record_size);
+    exit(EXIT_FAILURE);
+  }
+}
+static void  sort_input_files(
+    char *f1, struct stat *fs1,
+    char *f2, struct stat *fs2,
+    FileInfo *larger,
+    FileInfo *smaller,
+    off_t record_size)
+{
+  if (fs1->st_size > fs2->st_size) {
+    larger->name = f1;
+    larger->size = fs1->st_size;
+    smaller->name = f2;
+    smaller->size = fs2->st_size;
+  } else {
+    larger->name = f2;
+    larger->size = fs2->st_size;
+    smaller->name = f1;
+    smaller->size = fs1->st_size;
+  }
+  larger->count = larger->size / record_size;
+  smaller->count = smaller->size / record_size;
+}
+static int read_input(FileInfo *f, Part buffer, size_t record_size)
+{
+  FILE *stream;
+  if ((stream = fopen(f->name, "rb")) == NULL) {
+    print_error(errno,__FILE__, f->name);
+    return -1;;
+  }
+  bool read_failure = false;
+  if ((fread(buffer, record_size, f->count, stream) < f->count) || ferror(stream)) {
+    print_error(errno,__FILE__, f->name);
+    errno = 0;
+    read_failure = true;
+  }
+  if ((fclose(stream) == EOF) || read_failure) {
+    if (errno)
+      print_error(errno,__FILE__, f->name);
+    return -1;
+  }
+  return 0;
+}
+static int write_output(char *output_filename, Part out_buffer, size_t out_count, size_t record_size)
+{
+  FILE *ostream;
+  if ((ostream = fopen(output_filename, "wb")) == NULL) {
+    print_error(errno, __FILE__, output_filename);
+    return -1;
+  }
+  bool write_error = false;
+  if (fwrite(out_buffer, record_size, out_count, ostream) < out_count || ferror(ostream)) {
+    write_error = true;
+    print_error(errno, __FILE__, output_filename);
+  }
+  if (fclose(ostream) == EOF || write_error) {
+    print_error(errno, __FILE__, output_filename);
+    return -1;
+  }
+  return 0;
+}
+static size_t merge_buffers(Part in_buffer, size_t in_count, Part out_buffer, size_t out_count)
+{
+  size_t o, m;
+  for (size_t i = 0; i < in_count; i++) {
+    o = 0;
+    while ( o < out_count && out_buffer[o].number < in_buffer[i].number) o++;
+    if (out_buffer[o].number == in_buffer[i].number) {
+      if (strcmp(out_buffer[o].name, in_buffer[i].name) != 0) {
+        fprintf(stderr, "Part #%d: name conflict\t \"%s\" vs. \"%s\"\n",
+            out_buffer[o].number,
+            out_buffer[o].name,
+            in_buffer[i].name);
+        continue;
+      }
+      if (!change_part_on_hand(&out_buffer[o], in_buffer[i].on_hand)) {
+        fprintf(stderr, "Invalid new quantity on hand for Part #%d\n", out_buffer[o].number);
+      }
+    } else {
+      for (m = out_count; m > o; m--)
+        out_buffer[m] = out_buffer[m-1];
+      out_buffer[o] = in_buffer[i];
+      out_count++;
+    }
+  }
+  return out_count;
+}
 int main(int argc, char *argv[])
 {
   if (argc != 4)
     invocation_error(argv[0], "[input file 1] [input file 2] [output file]"); 
 
-  char *larger_filename, *smaller_filename, *output_filename = argv[3];
+  char *output_filename = argv[3];
   struct stat file1_stat, file2_stat;
+  FileInfo larger, smaller;
   off_t record_size = (off_t)get_part_record_size();
-  off_t smaller_size;
-  size_t larger_count, smaller_count;
 
-  if (stat(argv[1], &file1_stat) != 0)
-    exit_error(errno, argv[0], argv[1]);
+  validate_input_file(argv[1], &file1_stat, record_size);
+  validate_input_file(argv[2], &file2_stat, record_size);
 
-  if (file1_stat.st_size % record_size)
-    fprintf(stderr, "Corrupt file '%s': size must be multiple of %ld\n",
-        argv[1],
-        (size_t)record_size);
+  sort_input_files(argv[1], &file1_stat, argv[2], &file2_stat, &larger, &smaller, record_size);
 
-  if (stat(argv[2], &file2_stat) != 0)
-    exit_error(errno, argv[0], argv[2]);
-
-  if (file2_stat.st_size % record_size)
-    fprintf(stderr, "Corrupt file '%s': size must be multiple of %ld\n",
-        argv[2],
-        (size_t)record_size);
-
-  if (file1_stat.st_size > file2_stat.st_size) {
-    larger_filename = argv[1];
-    smaller_filename = argv[2];
-    smaller_size = file2_stat.st_size;
-    larger_count = file1_stat.st_size / record_size;
-    smaller_count = file2_stat.st_size / record_size;
-  } else {
-    larger_filename = argv[2];
-    smaller_filename = argv[1];
-    smaller_size = file1_stat.st_size;
-    larger_count = file2_stat.st_size / record_size;
-    smaller_count = file1_stat.st_size / record_size;
-  }
-
-  size_t combined_size = (size_t) file1_stat.st_size + file2_stat.st_size;
-  Part merge_buffer = (Part)malloc(combined_size);
-  if (!merge_buffer)
+  Part out_buffer = (Part)malloc(larger.size + smaller.size);
+  if (!out_buffer)
     memory_error(__FILE__, __LINE__, __func__);
 
-  Part other_buffer = malloc(smaller_size);
-  if (!other_buffer)
+  if (read_input(&larger, out_buffer, record_size) != 0) {
+    free(out_buffer);
+    exit(EXIT_FAILURE);
+  }
+
+  Part in_buffer = malloc(smaller.size);
+  if (!in_buffer) {
+    free(out_buffer);
     memory_error(__FILE__, __LINE__, __func__);
-
-  FILE *larger_stream, *smaller_stream;
-
-  if ((larger_stream = fopen(larger_filename, "rb")) == NULL) {
-    exit_error(errno,argv[0], larger_filename);
   }
 
-  if ((smaller_stream = fopen(smaller_filename, "rb")) == NULL) {
-    if (fclose(larger_stream) == EOF)
-      print_error(errno, __FILE__, larger_filename);
-    exit_error(errno,argv[0], smaller_filename);
-  }
-
-  if (fread(merge_buffer, record_size, larger_count, larger_stream) < larger_count) {
-    print_error(errno, argv[0], larger_filename);
-    if (fclose(larger_stream) == EOF)
-      print_error(errno, __FILE__, larger_filename);
-    if (fclose(smaller_stream) == EOF)
-      print_error(errno, __FILE__, smaller_filename);
+  if (read_input(&smaller, in_buffer, record_size) != 0) {
+    free(in_buffer);
+    free(out_buffer);
     exit(EXIT_FAILURE);
   }
-  if (fread(other_buffer, record_size, smaller_count, smaller_stream) < smaller_count) {
-    print_error(errno, argv[0], smaller_filename);
-    if (fclose(larger_stream) == EOF)
-      print_error(errno, __FILE__, larger_filename);
-    if (fclose(smaller_stream) == EOF)
-      print_error(errno, __FILE__, smaller_filename);
+
+  size_t out_count = merge_buffers(in_buffer, smaller.count, out_buffer, larger.count);
+  free(in_buffer);
+
+  if (write_output(output_filename, out_buffer, out_count, record_size) != 0) {
+    free(out_buffer);
     exit(EXIT_FAILURE);
   }
-  if (fclose(larger_stream) == EOF)
-    print_error(errno, __FILE__, larger_filename);
-  if (fclose(smaller_stream) == EOF)
-    exit_error(errno, __FILE__, smaller_filename);
 
-
-  for (size_t s = 0; s < smaller_count; s++) {
-    for (size_t l = 0; l < larger_count; l++) {
-      if ((merge_buffer + l)->number == (other_buffer + s)->number) {
-        if (strcmp((merge_buffer + l)->name, (other_buffer + s)->name) != 0) {
-          fprintf(stderr, "Part #%d: name conflict\n", (merge_buffer + l)->number);
-          continue;
-        }
-        if (!set_part_on_hand((merge_buffer + l), (other_buffer + s)->on_hand)) {
-          fprintf(stderr, "Invalid new quantity on hand for Part #%d\n", (merge_buffer + l)->number);
-          continue;
-        }
-      }
-    }
-  }
-
-  /*
-   * for each record in the smaller
-   *   check each part number in the larger until:
-   *      same number is found:
-   *        if name values differ
-   *          report to stderr
-   *          continue
-   *        combine in stock values
-   *        if the new value is invalid
-   *          report to stderr
-   *          continue
-   *      larger part number is found:
-   *        increment "added" counter
-   *        shift all subsequent elements in the larger buffer
-   *        copy record from smaller into the hole
-   *      we reach the end without finding a larger part number
-   *        increment "added" counter
-   *        copy record from smaller at end
-   *
-   * open the output file
-   * fwrite the output buffer
-   * close the output file
-   */
-
-
+  free(out_buffer);
   return 0;
 }
