e6490349c6b9c112d6221bc089bccb390e584b03
Tim Camper <twcamper@thoughtworks.com>
2013-05-05 22:40:37 -0500

    pgm 22.16, cp implemented with descriptor-level IO and 4096 instead of 512 block size

---
+++ 22/22.16-cp.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+#define BLOCK_SIZE 4096
+
+int main(int argc, char * argv[])
+{
+  int fd_source, fd_target, rc = 0;
+  ssize_t n_read;
+  unsigned char buffer[BLOCK_SIZE] = {0};
+  struct stat source_stat;
+
+  if (argc != 3) {
+    fprintf(stderr, "usage %s source_file target_file\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  if (stat(argv[1], &source_stat) != 0) {
+    perror("stat");
+    exit(EXIT_FAILURE);
+  }
+
+  if ((fd_source = open(argv[1], O_RDONLY)) == -1) {
+    perror(argv[1]);
+    exit(EXIT_FAILURE);
+  }
+
+  if ((fd_target = open(argv[2], O_WRONLY|O_CREAT)) == -1) {
+    perror(argv[2]);
+    if (close(fd_source) == -1)
+      perror(argv[1]);
+    exit(EXIT_FAILURE);
+  }
+
+  if (chmod(argv[2], source_stat.st_mode) == -1) {
+    perror("chmod");
+    exit(EXIT_FAILURE);
+  }
+
+  for (;;) {
+    if ((n_read = read(fd_source, buffer, BLOCK_SIZE)) == -1) {
+      perror(argv[1]);
+      exit(EXIT_FAILURE);
+    }
+    if (!n_read)
+      break;
+
+    if (write(fd_target, buffer, n_read) == -1) {
+      perror(argv[2]);
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  if (close(fd_source) == -1) {
+    perror(argv[1]);
+    rc = -1;
+  }
+
+  if (close(fd_target) == -1) {
+    perror(argv[2]);
+    rc = -1;
+  }
+
+  return rc;
+}
