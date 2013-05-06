fe9a6980bc76c60fb74b284a82f3b67e96523116
Tim Camper <twcamper@thoughtworks.com>
2013-05-06 08:38:15 -0500

    pgm 22.16, clean up the solution

--- 22/22.16-cp.c
+++ 22/22.16-cp.c
@@ -6,6 +6,12 @@
 
 #define BLOCK_SIZE 4096
 
+static void exit_error(char *s)
+{
+  perror(s);
+  exit(EXIT_FAILURE);
+}
+
 int main(int argc, char * argv[])
 {
   int fd_source, fd_target, rc = 0;
@@ -18,15 +24,11 @@ int main(int argc, char * argv[])
     exit(EXIT_FAILURE);
   }
 
-  if (stat(argv[1], &source_stat) != 0) {
-    perror("stat");
-    exit(EXIT_FAILURE);
-  }
+  if (stat(argv[1], &source_stat) != 0)
+    exit_error("stat");
 
-  if ((fd_source = open(argv[1], O_RDONLY)) == -1) {
-    perror(argv[1]);
-    exit(EXIT_FAILURE);
-  }
+  if ((fd_source = open(argv[1], O_RDONLY)) == -1)
+    exit_error(argv[1]);
 
   if ((fd_target = open(argv[2], O_WRONLY|O_CREAT)) == -1) {
     perror(argv[2]);
@@ -35,24 +37,14 @@ int main(int argc, char * argv[])
     exit(EXIT_FAILURE);
   }
 
-  if (chmod(argv[2], source_stat.st_mode) == -1) {
-    perror("chmod");
-    exit(EXIT_FAILURE);
-  }
+  if (chmod(argv[2], source_stat.st_mode) == -1)
+    exit_error("chmod");
 
-  for (;;) {
-    if ((n_read = read(fd_source, buffer, BLOCK_SIZE)) == -1) {
-      perror(argv[1]);
-      exit(EXIT_FAILURE);
-    }
-    if (!n_read)
-      break;
-
-    if (write(fd_target, buffer, n_read) == -1) {
-      perror(argv[2]);
-      exit(EXIT_FAILURE);
-    }
-  }
+  while ((n_read = read(fd_source, buffer, BLOCK_SIZE)) > 0)
+    if (write(fd_target, buffer, n_read) == -1)
+      exit_error(argv[2]);
+  if (n_read == -1)
+    exit_error(argv[1]);
 
   if (close(fd_source) == -1) {
     perror(argv[1]);
